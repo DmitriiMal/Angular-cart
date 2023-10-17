1c9d35f9596c8d285f6c548cc133992ed31dfe90
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-17 21:12:11 +0200

    create workspace: navbar, product-list, broduct-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    NavbarComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
